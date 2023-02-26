a14d3c02cd47ad656307ba74b2da1afb19c2dc59
小孟要吃肉 <10329613+smallmeng@user.noreply.gitee.com>
2023-02-26 14:07:29 +0000

    update readsb-install-buster.sh.
    Signed-off-by: 小孟要吃肉 <10329613+smallmeng@user.noreply.gitee.com>

--- readsb-install-buster.sh
+++ readsb-install-buster.sh
@@ -201,6 +201,9 @@ cd "$ipath"
 wget -O tar1090-install.sh https://gitee.com/smallmeng/tar1090-en/raw/master/install.sh
 bash tar1090-install.sh /run/readsb
 
+rm -rf /usr/local/share/tar1090
+git clone https://gitee.com/smallmeng/tar1090-cn /usr/local/share/tar1090
+
 rm -rf /etc/motd
 wget -P /etc https://gitee.com/smallmeng/files/raw/master/motd
 
