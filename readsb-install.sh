a84acfff42154f3b1746e139e60f95331e1c4f8b
cbh1987 <cbh1987@users.noreply.github.com>
2023-06-30 10:51:42 +0800

    Update readsb-install.sh

--- readsb-install.sh
+++ readsb-install.sh
@@ -190,11 +190,11 @@ chmod a+x /usr/local/bin/readsb-set-location
 echo --------------
 cd "$ipath"
 
-wget -O tar1090-install.sh https://gitee.com/smallmeng/tar1090-en/raw/master/install.sh
+wget -O tar1090-install.sh https://github.com/cbh1987/tar1090-en/raw/master/install.sh
 bash tar1090-install.sh /run/readsb
 
 rm -rf /usr/local/share/tar1090
-git clone https://gitee.com/smallmeng/tar1090-cn /usr/local/share/tar1090
+git clone https://github.com/cbh1987/tar1090-cn /usr/local/share/tar1090
 
 rm -rf /etc/motd
 wget -P /etc https://gitee.com/smallmeng/files/raw/master/motd
